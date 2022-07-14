142bc26f3ccfcd6ed14584ba5dc7449ce794ba9c
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-07-14 15:20:41 +0300

    start

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+(() => {
+    const refs = {
+        openMenuBtn: document.querySelector('.menu-open-btn'),
+        closeMenuBtn: document.querySelector('.mobil__button'),
+        menu: document.querySelector('.mobil'),
+        body: document.querySelector('body'),
+    }; refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu); function toggleMenu() {
+        refs.menu.classList.toggle('is-hidden');
+        refs.body.classList.toggle('no-scroll');
+    }
+})();
